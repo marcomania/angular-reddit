75ca864cb7f4bff72cee5ae432d61eddcbf401dd
marcomania <marco.untiveros@unmsm.edu.pe>
2021-10-29 14:41:26 -0500

    initial heroku deploy

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,32 @@
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row';
+  @Input() article: Article;
+
+  constructor() {
+    this.article = new Article(
+    'Angular constructor',
+    'http://angular.io',
+    10);
+  }
+  
+  voteUp(): boolean {
+    this.article.voteUp();
+    return false;
+  }
+  voteDown(): boolean {
+    this.article.voteDown();
+    return false;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
